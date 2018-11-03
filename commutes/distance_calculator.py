dfd3cee450fcb334d0abc983146d57c827bf7db7
Gabriela Valle <gabrieladvmininel@gmail.com>
2018-11-03 19:24:32 +0000

    Refactor distance calculator

--- commutes/distance_calculator.py
+++ commutes/distance_calculator.py
@@ -63,13 +63,14 @@ class CommuteDistanceCalculator:
         times_for_each_route: List[float] = []
         for route in list_of_routes:
             legs: list = route['legs']
+            posix_time: float = 0.0
             for leg in legs:
                 # Get time in POSIX seconds
-                posix_time: float = leg['duration']['value']
-                times_for_each_route.append(posix_time)
+                posix_time += leg['duration']['value']
+            times_for_each_route.append(posix_time)
         self.analyse_difference_of_commute_times_is_reasonable(times_for_each_route)
 
-    def analyse_difference_of_commute_times_is_reasonable(self, times_for_each_route: List[float]):
+    def analyse_difference_of_commute_times_is_reasonable(self, routes_times: List[float]):
         """
         Alerting method to assure that the user does not depend on a single commute route and that
         we can account for a reasonable time difference between them.
@@ -77,12 +78,12 @@ class CommuteDistanceCalculator:
         For the moment, it does not change the data or stop calculations; its main purpose is
         to observe the data processed and emit alerts.
         """
-        self.compare_min_and_max_difference_with_toleration(times_for_each_route)
-        self.compare_times_means_and_differences_with_toleration(times_for_each_route)
+        min_time, max_time = min(routes_times), max(routes_times)
+        self.compare_min_and_max_difference_with_toleration(min_time, max_time)
+        self.compare_times_means_and_differences_with_toleration(routes_times, min_time, max_time)
 
-    def compare_min_and_max_difference_with_toleration(self, times_for_each_route: List[float]):
+    def compare_min_and_max_difference_with_toleration(self, min_time: float, max_time: float):
         # Compares the difference between the maximum and the minimum time to the tolerated amount
-        min_time, max_time = min(times_for_each_route), max(times_for_each_route)
         if (max_time - min_time) > posix_tolerance_for_commute:
             LOG.warning(
                 "The difference encountered between the max and the min time for routes from %s "
@@ -93,15 +94,20 @@ class CommuteDistanceCalculator:
                 posix_tolerance_for_commute
             )
 
-    def compare_times_means_and_differences_with_toleration(self, times_for_each_route):
+    def compare_times_means_and_differences_with_toleration(
+            self,
+            times_for_each_route: List[float],
+            min_time: float,
+            max_time: float
+    ):
         analysis_list: List[float] = times_for_each_route.copy()
-        min_time, max_time = min(times_for_each_route), max(times_for_each_route)
         analysis_list.remove(min_time)
         analysis_list.remove(max_time)
 
         # Gets the mean of all other elements in the list to compare with this difference.
         avg_time_of_other_routes = sum(analysis_list) / len(analysis_list)
         abs_difference_of_routes_avgs = abs(avg_time_of_other_routes - (max_time - min_time / 2))
+
         # TODO: is this supposed to be compared to the tolerance for commute times difference?
         # Analyse further and maybe make a data analysis model for it.
         if abs_difference_of_routes_avgs > posix_tolerance_for_commute:
