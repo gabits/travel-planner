dc0b89a0d268e738901553c1eb9ce334810b07fa
Gabriela Valle <gabrieladvmininel@gmail.com>
2018-11-03 16:12:06 +0000

    Improve distance calculator and log errors when retrieving API data

--- commutes/distance_calculator.py
+++ commutes/distance_calculator.py
@@ -1,8 +1,11 @@
-# Ideas for the future:
-#
-# 1. class Place to handle coordinates (not stored in the db), which are more reliable than
-# str lookups
-#
+"""
+Ideas for the future:
+
+1. A class to handle coordinates (not stored in the db)
+2. Investigate how reliable is the str lookup for main transport places. Maybe send more
+information in the request to the Google API such as the country/city of location
+3. [IMPORTANT] Validate response from the API
+"""
 import requests
 
 from config.settings import GOOGLE_MAPS_API_URL, GOOGLE_MAPS_API_KEY
@@ -18,6 +21,13 @@ def calculate_time_of_commute(origin_name: str, destination_name: str):
         'key': GOOGLE_MAPS_API_KEY,
         'mode': 'transit',          # Only get public transport routes for now
     }
-    request = requests.get(url, params=query_params)
-    print(request.status_code)
-    return ''
+    result = requests.get(url, params=query_params)
+    data = result.json()
+    get_data_from_json(data, 'routes')
+
+
+def get_data_from_json(data: dict, key: str):
+    retrieved_key = data.get(key, None)
+    if not retrieved_key:
+        raise KeyError(f"Could not get key {key} from request to the API. Data received: {data}")
+    return retrieved_key
