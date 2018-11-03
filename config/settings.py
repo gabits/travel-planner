ac69c88aaadec71dadc3ea477e36481113886aae
Gabriela Valle <gabrieladvmininel@gmail.com>
2018-11-03 17:08:40 +0000

    GOOGLE_API_KEY gets picked up from dotenv in settings

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,16 @@
+"""
+This is where we define global variables that can be imported by every module in the application.
+"""
+import os
+
 from dotenv import load_dotenv, find_dotenv
 
+
+# Load values from .env file
 load_dotenv(find_dotenv())
 
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
+# Google API integration
 GOOGLE_MAPS_API_URL = "https://maps.googleapis.com"
-GOOGLE_MAPS_API_KEY = ""
+GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY', '')
